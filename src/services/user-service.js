c6a8bd90db2352730ae4f1477db38354777234be
anand <anandpromax@gmail.com>
2024-02-18 19:42:23 +0530

    Add dependencies and JWT functionality

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository = require("../repository/user-repository");
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/serverConfig");
 class UserService {
   constructor() {
     this.userRepository = new UserRepository();
@@ -30,5 +32,24 @@ class UserService {
       throw error;
     }
   }
+  createToken(user) {
+    try {
+      const response = jwt.sign(user, JWT_KEY, { expiresIn: '10h' });
+      return response;
+    } catch (error) {
+      console.log("something went wrong in the user service while creating token");
+      throw error;
+    }
+  }
+  verifyToken(token) {
+try {
+  const response = jwt.verify(token, JWT_KEY);
+  return response;
+} catch (error) {
+  console.log("something went wrong in the user service while verifying token");
+  throw error;
 }
+  }
+}
+
 module.exports = UserService;
